9738e94937408702b26c48be3cfcba58d576af34
Benjamin Tincher <b.h.tincher@gmail.com>
2021-06-30 13:09:50 -0600

    adds progressStatus file

---
+++ MsgMeCK/Helpers/ProgressStatus.swift
@@ -0,0 +1,30 @@
+//
+//  ProgressStatus.swift
+//  MsgMeCK
+//
+//  Created by Benjamin Tincher on 6/30/21.
+//
+
+import Foundation
+
+
+/*
+ 
+ PROGRESS:
+ - basic creating/fetching conversations, senders, and messages working
+ 
+ 
+ BUGS
+ -  New users working once completing create displayName?
+ 
+ 
+ TO DO:
+ -  Add listener to fetch convos on convolistVC
+ -  Add listener to fetch messages on conversationVC
+ -  calculate number of new messages on conversationVC
+ -  push notifications on messages
+ -  notifications run even when app is closed; update app badge
+ 
+ 
+ 
+ */
